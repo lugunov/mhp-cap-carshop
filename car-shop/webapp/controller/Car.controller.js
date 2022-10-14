242f703380d0d7eda6f0ce7aec216b666923e60a
Eike Vogel <DKXTTY0>
2022-10-14 10:01:01 +0000

    cars view

---
+++ car-shop/webapp/controller/Car.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/m/library"
+], function (BaseController, JSONModel, formatter, mobileLibrary) {
+    "use strict";
+
+    // shortcut for sap.m.URLHelper
+    var URLHelper = mobileLibrary.URLHelper;
+
+    return BaseController.extend("mhp.demo.carshop.controller.Car", {
+
+        formatter: formatter,
+
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
+
+        onInit: function () {
+            
+        }
+    });
+
+});
